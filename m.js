ec57052b6357adf5bb0d37781df59b53daa4ee53
Adhri14 <adhri.adly@gmail.com>
2023-01-27 23:55:52 +0800

    update syntax

--- m.js
+++ m.js
@@ -1,6 +1,13 @@
 const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');
 const fs = require('fs');
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 const SESSION_FILE_PATH = './session.json'
 
@@ -10,14 +17,13 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionConfig = require(SESSION_FILE_PATH)
 }
 
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root: __dirname})
+})
 
 const client = new Client({puppeteer: {headless: true}, session: sessionConfig});
 
-client.on('qr', (qr) => {
-    // Generate and scan this code with your phone
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, {small: true});
-});
+
 
 client.on('authenticated', session => {
   console.log('AUTHENTICATED', session)
@@ -29,9 +35,7 @@ client.on('authenticated', session => {
   })
 })
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+
 
 client.on('message', msg => {
     if (msg.body == '!ping') {
@@ -40,4 +44,21 @@ client.on('message', msg => {
 });
 
 client.initialize();
- 
+
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...')
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url)
+      socket.emit('message', 'QR Code received, scan please!')
+    })
+  });
+  client.on('ready', () => {
+    socket.emit('message', 'Whatsapp is ready')
+  });
+})
+
+server.listen(8000, function() {
+  console.log('App running in server http://localhost:'+8000)
+})
